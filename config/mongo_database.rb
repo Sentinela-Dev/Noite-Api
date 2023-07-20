3fb8fc5fa840bedd0e2435d3bfefdc2e77368d8d
lu-css <dossantossilvalucas963@gmail.com>
2023-07-20 11:37:23 -0300

    chore: config test database

--- config/mongo_database.rb
+++ config/mongo_database.rb
@@ -10,6 +10,8 @@ module MongoDatabase
     database_url = case ENV['RACK_ENV']
                    when 'production'
                      "mongodb+srv://#{ENV['MONGO_USERNAME']}:#{ENV['MONGO_PASSWORD']}@#{ENV['MONGO_HOST']}/#{database_name}?retryWrites=true&w=majority&authSource=admin"
+                   when 'test'
+                     "mongodb://root:example@127.0.0.1:27017/#{database_name}_test?serverSelectionTimeoutMS=2000&authSource=admin"
                    else
                      "mongodb://root:example@127.0.0.1:27017/#{database_name}_dev?serverSelectionTimeoutMS=2000&authSource=admin"
                    end
